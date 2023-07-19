affd8939c6585c405c21dffc43d35cac4409f8d9
cjsmi <ClarenceSmith90@gmail.com>
2023-07-18 22:02:39 -0400

    add google font, monochrome color scheme, under construction text

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import "./index.scss";
 "use string";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const navlinks = document.getElementById("navlinks").children
+    let activeLink = navlinks[0];
+
+
+    for(let i = 0; i < navlinks.length; i++){
+        const link = navlinks[i];
+
+        link.addEventListener('click', (e) => {
+            activeLink.classList.remove('active');
+            link.classList.add('active');
+            activeLink = link;
+        })
+    }
+})
