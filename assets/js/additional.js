abc09b8e87df9c989301355f53a421d42cd92383
cjsmi <ClarenceSmith90@gmail.com>
2023-11-15 19:39:23 -0500

    fix mobile view, fix projects text, remove debugger

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#gmail').addEventListener('click', (e) => {
         navigator.clipboard.writeText("ClarenceSmith90@gmail.com");
         const popup = document.querySelector('#copy');
-        debugger;
         popup.style.visibility = "visible";
         popup.style.opacity = "100";
         setTimeout(() => {popup.style.opacity = 0; setTimeout(()=>{popup.style.visibility="hidden"}, 500)}, 3000)
